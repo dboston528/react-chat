2658c5be6e5276aded212a38d3f742439c94498b
DeAndre Boston <dboston3@yahoo.com>
2019-05-24 20:32:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MessageList from './MessageList';
+import SendMessageForm from './SendMessageForm'
+import Chatkit from '@pusher/chatkit-client'
+import { testToken, instanceLocator, username, roomId } from './config'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const instanceLocator = "v1:us1:98cbd997-c757-47ca-b5ee-d0c9a83592e6"
+// const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/98cbd997-c757-47ca-b5ee-d0c9a83592e6/token"
+// const username = "dboston528"
+// const roomId = '19432521';
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      messages: []
+    }
+    this.sendSimpleMessage = this.sendSimpleMessage.bind(this)
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId: username,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: testToken
+      })
+    })
+
+    chatManager.connect()
+      .then(currentUser => {
+        this.currentUser = currentUser
+        this.currentUser.subscribeToRoom({
+          roomId: roomId,
+          hooks: {
+            onMessage: message => {
+              this.setState({
+                messages: [...this.state.messages, message]
+              })
+            }
+          }
+        })
+      })
+  }
+
+  sendSimpleMessage(text) {
+    this.currentUser.sendMessage({
+      text,
+      roomId: roomId
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p className='title'>Chat App</p>
+        <MessageList
+          messages={this.state.messages}
+          roomId={this.state.roomId}
+        />
+        <SendMessageForm sendMessage={this.sendSimpleMessage} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/MessageList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+
+
+class MessageList extends React.Component {
+    render() {
+        return (
+            <ul className="message-list">
+                {this.props.messages.map(message => {
+                    return (
+                        <li key={message.id} className="message">
+                            <div>
+                                {message.senderId}
+                            </div>
+                            <div>
+                                {message.text}
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+}
+
+export default MessageList;
